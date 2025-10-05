4a22cb25b42508f84f26fafa7ea2c94a1bc6d99b
Nicolina Wegert <nicolinawegert@gmail.com>
2025-10-05 13:39:59 +0200

    Bytte ut lång if/else-kedja mot en switch-sats för menyval, flyttade logik till separata metoder (AddUser, ListUsers, RemoveUser, SearchUser, ShowMenu)

--- Program.cs
+++ Program.cs
@@ -10,96 +10,118 @@ namespace Kodanalys
         static void Main(string[] args)
         {
             bool isRunning = true;
+
             while (isRunning)
             {
-                Console.WriteLine("Välj ett alternativ:");
-                Console.WriteLine("1. Lägg till användare");
-                Console.WriteLine("2. Visa alla användare");
-                Console.WriteLine("3. Ta bort användare");
-                Console.WriteLine("4. Sök användare");
-                Console.WriteLine("5. Avsluta");
+                ShowMenu();
                 string? menuChoice = Console.ReadLine();
 
-                if (menuChoice == "1")
+                switch (menuChoice)
                 {
-                    Console.Write("Ange namn: ");
-                    string? userName = Console.ReadLine();
-                    if (user < 10)
-                    {
-                        userList[user] = userName!;
-                        user++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Listan är full!");
-                    }
+                    case "1":
+                        AddUser();
+                        break;
+                    case "2":
+                        ListUsers();
+                        break;
+                    case "3":
+                        RemoveUser();
+                        break;
+                    case "4":
+                        SearchUser();
+                        break;
+                    case "5":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltigt val.");
+                        break;
                 }
-                else if (menuChoice == "2")
-                {
-                    Console.WriteLine("Användare:");
-                    for (int i = 0; i < user; i++)
-                    {
-                        Console.WriteLine(userList[i]);
-                    }
-                }
-                else if (menuChoice == "3")
-                {
-                    Console.Write("Ange namn att ta bort: ");
-                    string? userToRemove = Console.ReadLine();
-                    int userIndex = -1;
-                    for (int i = 0; i < user; i++)
-                    {
-                        if (userList[i] == userToRemove)
-                        {
-                            userIndex = i;
-                            break;
-                        }
-                    }
+            }
+        }
 
-                    if (userIndex != -1)
-                    {
-                        for (int i = userIndex; i < user - 1; i++)
-                        {
-                            userList[i] = userList[i + 1];
-                        }
-                        user--;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Användaren hittades inte.");
-                    }
-                }
-                else if (menuChoice == "4")
+        static void ShowMenu()
+        {
+            Console.WriteLine("Meny:");
+            Console.WriteLine("1. Lägg till användare");
+            Console.WriteLine("2. Lista användare");
+            Console.WriteLine("3. Ta bort användare");
+            Console.WriteLine("4. Sök användare");
+            Console.WriteLine("5. Avsluta");
+            Console.Write("Välj ett alternativ: ");
+        }
+                
+        static void AddUser()
+        {
+            Console.Write("Ange namn: ");
+            string? userName = Console.ReadLine();
+                if (user < 10)
                 {
-                    Console.Write("Ange namn att söka: ");
-                    string? searchName = Console.ReadLine();
-                    bool found = false;
-                    for (int i = 0; i < user; i++)
-                    {
-                        if (userList[i] == searchName)
-                        {
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (found)
-                    {
-                        Console.WriteLine("Användaren finns i listan.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Användaren hittades inte.");
-                    }
-                }
-                else if (menuChoice == "5")
-                {
-                    isRunning = false;
+                    userList[user] = userName!;
+                    user++;
                 }
                 else
                 {
-                    Console.WriteLine("Ogiltigt val.");
+                    Console.WriteLine("Listan är full!");
                 }
-                Console.WriteLine();
+        }
+
+        static void ListUsers()
+        {
+            Console.WriteLine("Användare:");
+                for (int i = 0; i < user; i++)
+                {
+                    Console.WriteLine(userList[i]);
+                }
+        }
+        static void RemoveUser()
+        {
+            Console.Write("Ange namn att ta bort: ");
+            string? userToRemove = Console.ReadLine();
+            int userIndex = -1;
+            for (int i = 0; i < user; i++)
+            {
+                if (userList[i] == userToRemove)
+                {
+                    userIndex = i;
+                    break;
+                }
+            }
+
+            if (userIndex != -1)
+            {
+                for (int i = userIndex; i < user - 1; i++)
+                {
+                    userList[i] = userList[i + 1];
+                }
+                user--;
+            }
+            else
+            {
+                Console.WriteLine("Användaren hittades inte.");
+            }
+        }
+
+        static void SearchUser()
+        {
+            Console.Write("Ange namn att söka: ");
+            string? searchName = Console.ReadLine();
+            bool found = false;
+            for (int i = 0; i < user; i++)
+            {
+                if (userList[i] == searchName)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine("Användaren finns i listan.");
+            }
+            else
+            {
+                Console.WriteLine("Användaren hittades inte.");
             }
         }
     }
